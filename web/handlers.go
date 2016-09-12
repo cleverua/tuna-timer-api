29b8f111187224a35b11211eb66bfb6c646c2051
Pavlo Lysov <pavlikus@gmail.com>
2016-09-13 00:11:36 +0300

    initial round of work on http stuff testing

--- web/handlers.go
+++ web/handlers.go
@@ -2,30 +2,52 @@ package web
 
 import (
 	"encoding/json"
-	"io"
+	"log"
 	"net/http"
+	"net/http/httputil"
 	"time"
 
+	"github.com/pavlo/slack-time/commands"
+	"github.com/pavlo/slack-time/data"
 	"github.com/pavlo/slack-time/utils"
 )
 
 // Handlers is a collection of net/http handlers to serve the API
 type Handlers struct {
-	env    *utils.Environment
-	status map[string]string
+	env                   *utils.Environment
+	status                map[string]string
+	commandLookupFunction func(slackCommand data.SlackCommand) (commands.Command, error)
 }
 
 // NewHandlers constructs a Handlers collection
 func NewHandlers(env *utils.Environment) *Handlers {
-	return &Handlers{env: env, status: map[string]string{
-		"env":     env.Name,
-		"version": env.AppVersion,
-	}}
+	return &Handlers{
+		env: env,
+		status: map[string]string{
+			"env":     env.Name,
+			"version": env.AppVersion,
+		},
+		commandLookupFunction: commands.Get,
+	}
 }
 
 // Timer handles Slack /timer command
 func (h *Handlers) Timer(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	slackCommand := data.SlackCommand{
+		ChannelID:   r.PostFormValue("channel_id"),
+		ChannelName: r.PostFormValue("channel_name"),
+		Command:     r.PostFormValue("command"),
+		ResponseURL: r.PostFormValue("response_url"),
+		TeamDomain:  r.PostFormValue("team_domain"),
+		TeamID:      r.PostFormValue("team_id"),
+		Text:        r.PostFormValue("text"),
+		Token:       r.PostFormValue("token"),
+		UserID:      r.PostFormValue("user_id"),
+		UserName:    r.PostFormValue("user_name"),
+	}
+
+	cmd, _ := h.commandLookupFunction(slackCommand)
+	cmd.Execute(h.env)
 }
 
 // Health handles a call for app health request
@@ -33,4 +55,13 @@ func (h *Handlers) Health(w http.ResponseWriter, r *http.Request) {
 	uptime := time.Since(h.env.CreatedAt)
 	h.status["uptime"] = uptime.String() //is it good or not if I modify the map here?
 	json.NewEncoder(w).Encode(h.status)
+}
+
+func dumpRequest(r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		log.Println("Fail to dump the request!")
+	}
+	log.Println("Dumping the request:")
+	log.Printf("%q", dump)
 }
---
+++ web/handlers_test.go
@@ -0,0 +1,90 @@
+package web
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/pavlo/slack-time/commands"
+	"github.com/pavlo/slack-time/data"
+	"github.com/pavlo/slack-time/utils"
+
+	. "gopkg.in/check.v1"
+)
+
+func TestHandlers(t *testing.T) { TestingT(t) }
+
+type TestHandlersSuite struct {
+	env *utils.Environment
+}
+
+var _ = Suite(&TestHandlersSuite{})
+
+func (s *TestHandlersSuite) TestTimer(c *C) {
+	v := url.Values{}
+	v.Set("token", "gIkuvaNzQIHg97ATvDxqgjtO")
+	v.Set("team_id", "T0001")
+	v.Set("team_domain", "example")
+	v.Set("channel_id", "C2147483705")
+	v.Set("channel_name", "test")
+	v.Set("user_id", "U2147483697")
+	v.Set("user_name", "Steve")
+	v.Set("command", "/timer")
+	v.Set("text", "start Convert the logotype to PNG")
+	v.Set("response_url", "https://hooks.slack.com/commands/1234/5678")
+
+	req, err := http.NewRequest("POST", "/timer", bytes.NewBufferString(v.Encode()))
+	if err != nil {
+		c.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;")
+
+	mockCmd := &mockCommand{executed: false}
+	handlers := NewHandlers(s.env)
+	handlers.commandLookupFunction = func(slackCommand data.SlackCommand) (commands.Command, error) {
+		c.Assert(slackCommand.ChannelID, Equals, "C2147483705")
+		c.Assert(slackCommand.ChannelName, Equals, "test")
+		c.Assert(slackCommand.Command, Equals, "/timer")
+		c.Assert(slackCommand.ResponseURL, Equals, "https://hooks.slack.com/commands/1234/5678")
+		c.Assert(slackCommand.TeamDomain, Equals, "example")
+		c.Assert(slackCommand.TeamID, Equals, "T0001")
+		c.Assert(slackCommand.Text, Equals, "start Convert the logotype to PNG")
+		c.Assert(slackCommand.Token, Equals, "gIkuvaNzQIHg97ATvDxqgjtO")
+		c.Assert(slackCommand.UserID, Equals, "U2147483697")
+		c.Assert(slackCommand.UserName, Equals, "Steve")
+		return mockCmd, nil
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(handlers.Timer)
+
+	handler.ServeHTTP(recorder, req)
+	c.Assert(mockCmd.executed, Equals, true)
+}
+
+type mockCommand struct {
+	executed bool
+}
+
+func (cmd *mockCommand) Execute(env *utils.Environment) *commands.CommandResult {
+	cmd.executed = true
+	return nil
+}
+
+// Suite lifecycle and callbacks
+func (s *TestHandlersSuite) SetUpSuite(c *C) {
+	e := utils.NewEnvironment(utils.TestEnv, "1.0.0")
+	e.MigrateDatabase()
+
+	s.env = e
+}
+
+func (s *TestHandlersSuite) TearDownSuite(c *C) {
+	s.env.ReleaseResources()
+}
+
+func (s *TestHandlersSuite) SetUpTest(c *C) {
+	utils.TruncateTables(s.env)
+}
